707c6dbcb9f298b638f1a6e38ffe8200161d846e
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-10 23:11:56 +0200

    Module 19: Conversion

---
+++ convert.js
@@ -0,0 +1,18 @@
+//  ! Convert Inch to Feet
+function inchToFeet(inch) {
+  const feetFraction = inch / 12;
+  const feet = Math.floor(feetFraction);
+  const inches = Math.round((feetFraction - feet) * 12);
+  //   return `${feet}' ${inches}"`;
+  if (inches != 0) {
+    if (inches === 1) {
+      return `${feet} Feet ${inches} Inch`;
+    } else {
+      return `${feet} Feet ${inches} Inches`;
+    }
+  } else {
+    return `${feet} Feet`;
+  }
+}
+
+console.log(inchToFeet(38)); // Example usage, returns "3' 0""
