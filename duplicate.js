b6d6326d91d690e57e96c45f49b51eda185151d8
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-11 00:25:55 +0200

    Module 19: Unique Array

---
+++ duplicate.js
@@ -0,0 +1,16 @@
+// 1 Finding Duplicate Numbers in an Array
+
+function noDuplicate(arr) {
+  const unique = [];
+  for (item of arr) {
+    if (!unique.includes(item)) {
+      unique.push(item);
+    }
+  }
+  return unique;
+}
+
+const numbers = [1, 2, 3, 4, 5, 1, 2, 3];
+const friends = ["Alice", "Bob", "Alice", "Charlie", "Bob", "David"];
+console.log(noDuplicate(numbers));
+console.log(noDuplicate(friends));
